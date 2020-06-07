48a3eef811d7719785377634dc0632200d6f20bb
baoray <lunkute35222@gmail.com>
2020-06-07 14:30:10 +0700

    update file index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello Ma Fren!"));
+app.get("/users", (req, res) => res.send("Do ma may con cho !"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
